20e7834c9865ae9f2c6ed657d029970d098da9a1
C C Nipun Das <nipundas02@gmail.com>
2021-11-29 21:40:04 +0530

    update

--- OOPJ Lab/03. Inheritance of Employee.java
+++ OOPJ Lab/03. Inheritance of Employee.java
@@ -1,5 +1,14 @@
 package com.company;
 
+/*Write a Java program which creates a class named 'Employee' having the
+following members: Name, Age, Phone number, Address, Salary. It also has a
+method named 'print- Salary( )' which prints the salary of the Employee. Two
+classes 'Officer' and 'Manager' inherits the 'Employee' class. The 'Officer' and
+'Manager' classes have data members 'specialization' and 'department'
+respectively. Now, assign name, age, phone number, address and salary to an
+officer and a manager by making an object of both of these classes and print the
+same. (Exercise to understand inheritance).*/
+
 class Employee {
     String name;
     int age;
