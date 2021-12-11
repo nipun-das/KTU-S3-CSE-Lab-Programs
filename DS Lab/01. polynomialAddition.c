4c821593dcd10d9547927f9b3d6a9e80a9893ae6
C C Nipun Das <nipundas02@gmail.com>
2021-12-11 23:03:54 +0530

    Update 01. polynomialAddition.c

--- DS Lab/01. polynomialAddition.c
+++ DS Lab/01. polynomialAddition.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-
 /*Write a program to read two polynomials and store them in an array. Calculate the sum of the
 two polynomials and display the first polynomial, second polynomial and the resultant
 polynomial.*/
