f3d01520f7cbfba753babe278cd5b24ca8a54420
C C Nipun Das <nipundas02@gmail.com>
2022-01-06 09:56:21 +0530

    Update Sparse Matrix Sum and Fast Transpose.c

--- DS Lab/EXPERIMENT 01/Sparse Matrix Sum and Fast Transpose.c
+++ DS Lab/EXPERIMENT 01/Sparse Matrix Sum and Fast Transpose.c
@@ -1,3 +1,9 @@
+/*Write a program to enter two matrices in normal form . Write a function to convert two
+matrices to tuple form and display it. Also find the transpose of the two matrices represented
+in tuple form and display it. Find the sum of the two matrices in tuple form and display the
+sum in tuple form.*/
+
+
 #include <stdio.h>
 #define MAX 100
 #define MAX_COL 30
