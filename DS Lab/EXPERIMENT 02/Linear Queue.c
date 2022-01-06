eb0c49e8c7078f8d9a670c3bec34862880ca21d6
C C Nipun Das <nipundas02@gmail.com>
2022-01-06 09:58:50 +0530

    Update Linear Queue.c

--- DS Lab/EXPERIMENT 02/Linear Queue.c
+++ DS Lab/EXPERIMENT 02/Linear Queue.c
@@ -1,3 +1,11 @@
+/*
+Implement a Queue using arrays with the operations:
+1.Insert elements to the Queue.
+2.Delete elements from the Queue.
+3.Display the contents of the Queue after each operation.
+*/
+
+
 #include <stdio.h>
 #define MAX 5
 
