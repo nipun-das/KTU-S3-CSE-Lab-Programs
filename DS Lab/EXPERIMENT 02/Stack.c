10a0b4585b221b4817d2d26f652a9047785cc52d
C C Nipun Das <nipundas02@gmail.com>
2022-01-24 14:25:05 +0530

    Update Stack.c

--- DS Lab/EXPERIMENT 02/Stack.c
+++ DS Lab/EXPERIMENT 02/Stack.c
@@ -4,40 +4,38 @@ Implement a Stack using arrays with the operations:
 2.Popping elements from the Stack
 3.Display the contents of the Stack after each operation.
 */
-#include <stdio.h>
-#define MAX 8
 
-int stack[MAX], top = -1;
+#include <stdio.h>
+
+int stack[100], n, top = -1;
 
 void display()
 {
+
     if (top == -1)
     {
-        printf("Stack underflow");
+        printf("\nStack is empty");
     }
     else
     {
+        printf("\nStack : ");
         for (int i = 0; i <= top; i++)
         {
             printf("%d ", stack[i]);
         }
     }
 }
-
 void push(int item)
 {
-    if (top == MAX - 1)
+    if (top == n - 1)
     {
         printf("\nStack overflow");
     }
     else
     {
         stack[++top] = item;
-        printf("Stack : ");
-        display();
     }
 }
-
 void pop()
 {
     int del;
@@ -49,20 +47,21 @@ void pop()
     {
         del = stack[top--];
         printf("\nDeleted: %d\n", del);
-        printf("Stack : ");
-        display();
     }
 }
-
 int main()
 {
     int ch, element;
+    printf("\nEnter the size of stack ");
+    scanf("%d", &n);
+    int stack[n];
     do
     {
         printf("\n\nChoose operation");
         printf("\n1.Push");
         printf("\n2.Pop");
-        printf("\n3.Exit\n");
+        printf("\n3.Display");
+        printf("\n4.Exit\n");
         scanf("%d", &ch);
         switch (ch)
         {
@@ -75,10 +74,13 @@ int main()
             pop();
             break;
         case 3:
+            display();
+            break;
+        case 4:
             break;
         default:
             printf("Invalid Choice!");
             break;
         }
-    } while (ch < 3);
+    } while (ch < 4);
 }
