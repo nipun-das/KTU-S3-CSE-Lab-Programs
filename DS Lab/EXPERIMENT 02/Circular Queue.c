f40a9f88cb606a4474ee134ef04aede42f624d60
C C Nipun Das <nipundas02@gmail.com>
2022-01-24 14:25:56 +0530

    Update Circular Queue.c

--- DS Lab/EXPERIMENT 02/Circular Queue.c
+++ DS Lab/EXPERIMENT 02/Circular Queue.c
@@ -6,28 +6,27 @@ Implement a circular queue using arrays with the operations:
 */
 
 #include <stdio.h>
-#define MAX 5
 
-int cQ[MAX], front = -1, rear = -1;
+int cQ[100], front = -1, n, rear = -1;
 
 void display()
 {
+    printf("\nCircular Queue : ");
     int i = front;
     if (front == -1 && rear == -1)
     {
-        printf("Queue underflow\n");
+        printf("\nQueue is empty\n");
     }
     while (i != rear)
     {
         printf("%d ", cQ[i]);
-        i = (i + 1) % MAX;
+        i = (i + 1) % n;
     }
     printf("%d ", cQ[rear]);
 }
-
 void enqueue(int item)
 {
-    if ((rear + 1) % MAX == front)
+    if ((rear + 1) % n == front)
     {
         printf("\nQueue overflow\n");
     }
@@ -35,19 +34,14 @@ void enqueue(int item)
     {
         front = rear = 0;
         cQ[rear] = item;
-        printf("Circular Queue : ");
-        display();
     }
     else
     {
-        rear = (rear + 1) % MAX;
+        rear = (rear + 1) % n;
         cQ[rear] = item;
-        printf("Circular Queue : ");
-        display();
     }
 }
-
-void pop()
+void dequeue()
 {
     int del;
     if (front == -1 && rear == -1)
@@ -59,28 +53,27 @@ void pop()
         del = cQ[front];
         printf("The element removed is %d\n", del);
         front = rear = -1;
-        printf("Circular Queue : ");
-        display();
     }
     else
     {
         del = cQ[front];
         printf("The element removed is %d\n", del);
-        front = (front + 1) % MAX;
-        printf("Circular Queue : ");
-        display();
+        front = (front + 1) % n;
     }
 }
-
 int main()
 {
     int ch, element;
+    printf("\nEnter the size of the circular queue : ");
+    scanf("%d", &n);
+    int cQ[n];
     do
     {
         printf("\n\nChoose operation\n");
         printf("\n1.Enqueue");
         printf("\n2.Dequeue");
-        printf("\n3.Exit\n");
+        printf("\n3.Display");
+        printf("\n4.Exit\n");
         scanf("%d", &ch);
         switch (ch)
         {
@@ -90,13 +83,16 @@ int main()
             enqueue(element);
             break;
         case 2:
-            pop();
+            dequeue();
             break;
         case 3:
+            display();
+            break;
+        case 4:
             break;
         default:
             printf("Invalid Choice!");
             break;
         }
-    } while (ch < 3);
+    } while (ch < 4);
 }
