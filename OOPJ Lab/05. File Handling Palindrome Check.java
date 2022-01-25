05d8ec870b38a10b011af5fdc62027d741c68019
C C Nipun Das <nipundas02@gmail.com>
2022-01-25 10:04:05 +0530

    Update 05. File Handling Palindrome Check.java

--- OOPJ Lab/05. File Handling Palindrome Check.java
+++ OOPJ Lab/05. File Handling Palindrome Check.java
@@ -1,53 +1,60 @@
 /*Write a program to write a string to a file, read the string from the file and check whether it is a palindrome or not. Write the output to another file*/
 
-import java.io.*;
 import java.util.Scanner;
+import java.io.*;
 
-public class FileHandlingPalindromeReadAndWrite {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        FileWriter fw = null;
-        FileReader fr = null;
-        String s1, s2 = "";
-        System.out.println("Enter the string : ");
-        s1 = sc.nextLine();
-        try {
-            fw = new FileWriter("first.txt");
-            for (int i = 0; i < s1.length(); i++) {
-                fw.write(s1.charAt(i));
-            }
-            fw.close();
-            System.out.println("Write successful!");
-            fr = new FileReader("first.txt");
-            Scanner sr = new Scanner(fr);
-            while (sr.hasNextLine()) {
-                String c = sr.nextLine();
-                System.out.println("Contents of first.txt is " + c);
-            }
-            fr.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File is not found!");
-        }
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            s2 = s2 + s1.charAt(i);
-        }
-        try {
-            fw = new FileWriter("second.txt");
-            if (s1.equals(s2)) {
-                fw.write(s1 + " is palindrome");
-            } else {
-                fw.write(s1 + " is not a palindrome");
-            }
-            fw.close();
-            fr = new FileReader("second.txt");
-            Scanner sr = new Scanner(fr);
-            while (sr.hasNextLine()) {
-                String d = sr.nextLine();
-                System.out.println("Contents of second.txt is : " + d);
-            }
-            fr.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File is not found!");
-        }
-    }
+class PalindromeFile{
+	public static void main(String []args) throws IOException{
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the String : ");
+		String s1 = sc.nextLine();
+		FileWriter fw = null;
+		FileReader fr = null;
+		int i;
+		try{
+			fw = new FileWriter("first.txt");
+			fr = new FileReader("first.txt");
+			fw.write(s1); 
+			System.out.println("\nfirst.txt file is created!");
+			fw.close();
+			
+			System.out.print("Data in first.txt : ");
+			while((i = fr.read())!=-1){
+				System.out.print((char)i);
+			}
+			System.out.println();
+			fr.close();
+		}
+		catch(FileNotFoundException e){
+			System.out.println("File creation failed!");
+		}
+
+		String s2 ="" ;
+		for(i = s1.length()-1 ;i >= 0 ;i--){
+			s2 = s2 + s1.charAt(i);
+		}
+
+		try{
+			fw = new FileWriter("second.txt");
+			fr = new FileReader("second.txt");			
+			if(s1.equals(s2)){
+				fw.write(s1 + " is Palindrome");
+			}
+			else{
+				fw.write(s1 + " is not a Palindrome");
+			}
+			System.out.println("\nsecond.txt file is created!");
+			fw.close();
+			System.out.print("Data in second.txt : ");
+			while((i = fr.read())!=-1){
+				System.out.print((char)i);
+			}
+			System.out.println();
+			fr.close();	
+		}
+		catch(FileNotFoundException e){
+			System.out.println("File creation failed!");
+		}
+	}
 }
