33484a8465e74c35b1907d12e37e4ba3f0506cd5
C C Nipun Das <nipundas02@gmail.com>
2022-03-05 19:32:27 +0530

    Update 12. Quick Sort of names.java

--- OOPJ Lab/12. Quick Sort of names.java
+++ OOPJ Lab/12. Quick Sort of names.java
@@ -1,51 +1,56 @@
 import java.util.Scanner;
+
 public class QuickSort {
- int partition(String A[], int lb, int ub) {
- String pivot, temp;
- int start, end;
- pivot = A[lb];
- start = lb;
- end = ub;
- while (start < end) {
- while (A[start].compareTo(pivot) <= 0 && start < ub) {
- start++;
- }
- while (A[end].compareTo(pivot) > 0) {
- end--;
- }
- if (start < end) {
- temp = A[start];
- A[start] = A[end];
- A[end] = temp;
- }
- }
- A[lb] = A[end];
- A[end] = pivot;
- return end;
- }
- void quickSort(String A[], int lb, int ub) {
- int loc;
- if (lb < ub) {
- loc = partition(A, lb, ub);
- quickSort(A, lb, loc - 1);
- quickSort(A, loc + 1, ub);
- }
- }
- public static void main(String[] args) {
- Scanner sc = new Scanner(System.in);
- int i;
- System.out.print("Enter the number of the names: ");
- int n = sc.nextInt();
- String A[] = new String[n];
- System.out.print("Enter the " + n + " names: ");
- for (i = 0; i < n; i++) {
- A[i] = sc.next();
- }
- QuickSort obj = new QuickSort();
- obj.quickSort(A, 0, n - 1);
- System.out.print("The Sorted Names are : ");
- for (i = 0; i < n; i++) {
- System.out.print(A[i] + " ");
- }
- }
+
+    int partition(String A[], int lb, int ub) {
+        String pivot, temp;
+        int start, end;
+
+        pivot = A[lb];
+        start = lb;
+        end = ub;
+        while (start < end) {
+            while (A[start].compareTo(pivot) <= 0 && start < ub) {
+                start++;
+            }
+            while (A[end].compareTo(pivot) > 0) {
+                end--;
+            }
+            if (start < end) {
+                temp = A[start];
+                A[start] = A[end];
+                A[end] = temp;
+            }
+        }
+        A[lb] = A[end];
+        A[end] = pivot;
+        return end;
+    }
+
+    void quickSort(String A[], int lb, int ub) {
+        int loc;
+        if (lb < ub) {
+            loc = partition(A, lb, ub);
+            quickSort(A, lb, loc - 1);
+            quickSort(A, loc + 1, ub);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        System.out.print("Enter the number of the names: ");
+        int n = sc.nextInt();
+        String A[] = new String[n];
+        System.out.print("Enter the " + n + " names: ");
+        for (i = 0; i < n; i++) {
+            A[i] = sc.next();
+        }
+        QuickSort obj = new QuickSort();
+        obj.quickSort(A, 0, n - 1);
+        System.out.print("The Sorted Names are :  ");
+        for (i = 0; i < n; i++) {
+            System.out.print(A[i] + "  ");
+        }
+    }
 }
